fa38f08ca3ccb81a82b3733cfaab95943df647d0
thanhdat2k2 <thanhdat2k2.810@gmail.com>
2025-10-16 11:48:31 +0700

    fix projects section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const menuIcon = document.getElementById("menu-icon");
 const navLinks = document.getElementById("nav-links");
+const links = document.querySelectorAll(".nav-links li a");
 
 menuIcon.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-
   // Đổi icon giữa bars ↔ X
   const icon = menuIcon.querySelector("i");
   icon.classList.toggle("fa-bars");
   icon.classList.toggle("fa-xmark");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+  });
 });
